43d15d47f8bd2e2ccd9c02ae4433f28d628e5807
Thais Menezes <thais.mas@hotmail.com>
2025-02-03 16:25:47 -0300

    Initial Commit

---
+++ Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Thais Souza on 03/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
